b9507d08e2daf11fcb8a018e677b6ec850516693
Nathalia de Bona <nathalia.contactdc@gmail.com>
2026-06-24 12:24:15 -0300

    atualizado css e js pagina sobre

--- lumiere-atelie/script.js
+++ lumiere-atelie/script.js
@@ -7,3 +7,29 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scrolled');
   }
 });
+
+document.querySelectorAll('.produto-card').forEach(card => {
+  const imgs = card.querySelectorAll('.produto-imagens img');
+  const dots = card.querySelectorAll('.dot');
+
+  if (imgs.length <=1) return;
+
+  let atual = 0;
+
+  imgs.forEach((img, i) => {
+    img.style.display = i === 0 ? 'block' : 'none';
+  });
+
+  dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => {
+
+      imgs[atual].style.display = 'none';
+      dots[atual].classList.remove('ativa');
+
+      atual = i;
+
+      imgs[atual].style.display = 'block';
+      dots[atual].classList.add('ativa');
+    })
+  })
+})
